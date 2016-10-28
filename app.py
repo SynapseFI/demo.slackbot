1079b143181bcff3b9ad633babc5e936c1e0f34a
stevula <brodericksteven@gmail.com>
2016-10-27 17:12:32 -0700

    Disabled sqlalchemy setting and add outer loop so 429 won't break loop

--- app.py
+++ app.py
@@ -8,16 +8,18 @@ from bot import slack_client, parse_slack_output, handle_command
 # configure Flask and postgres
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # second delay between reading from firehose
-READ_WEBSOCKET_DELAY = 1
-if slack_client.rtm_connect():
-    print('StarterBot connected and running!')
-    while True:
-        command, channel = parse_slack_output(slack_client.rtm_read())
-        if command and channel:
-            handle_command(command, channel)
-        time.sleep(READ_WEBSOCKET_DELAY)
-else:
-    print('Connection failed. Invalid Slack token or bot ID?')
+while True:
+    READ_WEBSOCKET_DELAY = 1
+    if slack_client.rtm_connect():
+        print('StarterBot connected and running!')
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print('Connection failed. Invalid Slack token or bot ID?')
