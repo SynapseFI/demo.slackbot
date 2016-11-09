e8e12d9668135c19fa63b92b9f9cdf256fec7057
stevula <brodericksteven@gmail.com>
2016-11-09 15:08:45 -0800

    Move User creation logic from routes to User model

--- synapse_slackbot/models.py
+++ synapse_slackbot/models.py
@@ -1,5 +1,8 @@
 """SQLAlchemy model class definitions."""
+from synapse_pay_rest import User as SynapseUser
+from synapse_pay_rest.models.nodes import AchUsNode, SynapseUsNode
 from .config import db
+from .synapse_client import synapse_client
 
 
 class User(db.Model):
@@ -22,6 +25,78 @@ class User(db.Model):
     def __repr__(self):
         return '<User: (slack %r), (synapse: %r)>' % (self.slack_user_id,
                                                       self.synapse_user_id)
+
+    @classmethod
+    def from_request(cls, slack_id, request):
+        synapse_user = cls.create_synapse_user(slack_id, request)
+        cls.submit_cip(synapse_user, request)
+        debit_node = cls.create_debit_node(synapse_user, request)
+        savings_node = cls.create_savings_node(synapse_user, request)
+        user = cls(slack_id, synapse_user.id, debit_node.id, savings_node.id)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    @staticmethod
+    def create_synapse_user(slack_id, request):
+        """Creates a new user with Synapse."""
+        # these 'options' actually required until pending API update or lib change
+        options = {
+            'note': 'created by Synapse Slackbot',
+            'supp_id': slack_id,
+            'is_business': False,
+            'cip_tag': 1
+        }
+
+        synapse_user = SynapseUser.create(client=synapse_client,
+                                          email=request.form['email'],
+                                          phone_number=request.form['phone'],
+                                          legal_name=request.form['name'].title(),
+                                          **options)
+        return synapse_user
+
+    @staticmethod
+    def submit_cip(synapse_user, request):
+        """Uploads CIP base document, photo ID, and SSN for user to Synapse."""
+        year, month, day = request.form['birthday'].split('-')
+        base_doc = synapse_user.add_base_document(
+            ip='127.0.0.1',
+            name=request.form['name'],
+            alias=request.form['name'],
+            birth_day=int(day),
+            birth_month=int(month),
+            birth_year=int(year),
+            email=request.form['email'],
+            phone_number=request.form['phone'],
+            entity_type='NOT_KNOWN',
+            entity_scope='Not Known',
+            address_street=request.form['address_street'],
+            address_city=request.form['address_city'],
+            address_subdivision=request.form['address_state'].upper(),
+            address_postal_code=request.form['address_zip'],
+            address_country_code='US')
+        base_doc.add_virtual_document(type='SSN', value=request.form['ssn'])
+        img_file = request.files['govt_id']
+        base_doc.add_physical_document(type='GOVT_ID', mime_type='image/jpeg',
+                                       byte_stream=img_file.read())
+
+    @staticmethod
+    def create_debit_node(synapse_user, request):
+        """Creates a node from which to draw funds for savings."""
+        return AchUsNode.create(
+            synapse_user,
+            account_number=request.form['account_number'],
+            routing_number=request.form['routing_number'],
+            nickname='Synapse Automatic Savings Debit Account',
+            account_type='PERSONAL',
+            account_class='CHECKING'
+        )
+
+    @staticmethod
+    def create_savings_node(synapse_user, form_data):
+        """Creates an FDIC-insured account at Triumph Bank to deposit savings."""
+        return SynapseUsNode.create(synapse_user,
+                                    nickname='Synapse Automatic Savings Account')
 
 
 class RecurringTransaction(db.Model):
--- synapse_slackbot/run.py
+++ synapse_slackbot/run.py
@@ -4,11 +4,8 @@ import time
 import os
 import _thread
 from flask import render_template, request
-from synapse_pay_rest import User as SynapseUser
-from synapse_pay_rest.models.nodes import AchUsNode, SynapseUsNode
 from .config import app, db, slack_client
 from .app.synapse_bot import SynapseBot
-from .synapse_client import synapse_client
 from .models import User
 
 synapse_bot = SynapseBot(slack_client, os.environ.get('SLACKBOT_ID'))
@@ -20,87 +17,8 @@ def register(slack_id):
     if request.method == 'GET':
         return render_template('register.html', slack_id=slack_id)
     elif request.method == 'POST':
-        user = process_form(slack_id, request)
+        user = User.from_request(slack_id, request)
         return str(user)
-
-
-def process_form(slack_id, request):
-    """Creates the user and nodes from form data."""
-    synapse_user = create_synapse_user(slack_id, request)
-    submit_cip(synapse_user, request)
-    debit_node = create_debit_node(synapse_user, request)
-    savings_node = create_savings_node(synapse_user, request)
-    user = create_user(slack_id, synapse_user.id, debit_node.id, savings_node.id)
-    return user
-
-
-def create_synapse_user(slack_id, form_data):
-    """Creates a new user with Synapse."""
-    # these 'options' actually required until pending API update or lib change
-    options = {
-        'note': 'created by Synapse Slackbot',
-        'supp_id': slack_id,
-        'is_business': False,
-        'cip_tag': 1
-    }
-
-    synapse_user = SynapseUser.create(client=synapse_client,
-                                      email=request.form['email'],
-                                      phone_number=request.form['phone'],
-                                      legal_name=request.form['name'].title(),
-                                      **options)
-    return synapse_user
-
-
-def submit_cip(synapse_user, request):
-    """Uploads CIP base document, photo ID, and SSN for user to Synapse."""
-    year, month, day = request.form['birthday'].split('-')
-    base_doc = synapse_user.add_base_document(
-        ip='127.0.0.1',
-        name=request.form['name'],
-        alias=request.form['name'],
-        birth_day=int(day),
-        birth_month=int(month),
-        birth_year=int(year),
-        email=request.form['email'],
-        phone_number=request.form['phone'],
-        entity_type='NOT_KNOWN',
-        entity_scope='Not Known',
-        address_street=request.form['address_street'],
-        address_city=request.form['address_city'],
-        address_subdivision=request.form['address_state'].upper(),
-        address_postal_code=request.form['address_zip'],
-        address_country_code='US')
-    base_doc.add_virtual_document(type='SSN', value=request.form['ssn'])
-    img_file = request.files['govt_id']
-    base_doc.add_physical_document(type='GOVT_ID', mime_type='image/jpeg',
-                                   byte_stream=img_file.read())
-
-
-def create_debit_node(synapse_user, request):
-    """Creates a node from which to draw funds for savings."""
-    return AchUsNode.create(
-        synapse_user,
-        account_number=request.form['account_number'],
-        routing_number=request.form['routing_number'],
-        nickname='Synapse Automatic Savings Debit Account',
-        account_type='PERSONAL',
-        account_class='CHECKING'
-    )
-
-
-def create_savings_node(synapse_user, form_data):
-    """Creates an FDIC-insured account at Triumph Bank to deposit savings."""
-    return SynapseUsNode.create(synapse_user,
-                                nickname='Synapse Automatic Savings Account')
-
-
-def create_user(slack_id, synapse_id, debit_node_id, savings_node_id):
-    """Create a user with Slack and Synapse info in the database."""
-    user = User(slack_id, synapse_id, debit_node_id, savings_node_id)
-    db.session.add(user)
-    db.session.commit()
-    return user
 
 
 def start_bot_event_loop():
