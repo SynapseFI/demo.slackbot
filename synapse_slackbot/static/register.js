c833222b33f73a66a5e75e1da3e7f845cc98da48
stevula <brodericksteven@gmail.com>
2016-11-11 19:59:49 -0800

    Move static files into folder

--- synapse_slackbot/static/register.js
+++ synapse_slackbot/static/register.js
@@ -9,16 +9,37 @@ const bindListeners = function() {
   $('.back').click(function() {
     if (activeTab > 0) {
       $('.'+tabs[activeTab]).removeClass('active')
+      if (activeTab === 2) {
+        $('.next').removeClass('inactive')
+        $('.submit').addClass('inactive')
+      }
       activeTab -= 1;
       $('.'+tabs[activeTab]).addClass('active')
     }
-  })
+  });
 
   $('.next').click(function() {
     if (activeTab < 2) {
       $('.'+tabs[activeTab]).removeClass('active')
       activeTab += 1;
       $('.'+tabs[activeTab]).addClass('active')
+      if (activeTab === 2) {
+        $('.next').addClass('inactive')
+        $('.submit').removeClass('inactive')
+      }
     }
-  })
+  });
+
+  $('form').submit(function() {
+    const url = this.attr('action')
+    debugger
+    const request = $.ajax({
+      url: url,
+      method: 'POST',
+      data: this.serialize(),
+      dataType: 'json'
+    });
+  });
 };
+
+
